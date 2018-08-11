de12cefdeace01daed375405bbe3d4b2155e388d
Nirav Desai <ndesai187@gmail.com>
2018-08-11 15:14:41 +0800

    adding plots with multiple y axis, refactoring code

--- src/plot_scatter.py
+++ src/plot_scatter.py
@@ -15,6 +15,21 @@ def create_frame(years_x_axis, country_list, country_data_y_axis):
     return df
 
 
+def create_frame_3x(years_x_axis, country_list, country_data_y_axis, x_df):
+    mux = pd.MultiIndex.from_product([x_df['Country'].unique(),range(x_df['Financial closure year'].min(), x_df['Financial closure year'].max() + 1)], names=['Country', 'Financial closure year'])
+
+    x_df_n = x_df.set_index(['Country', 'Financial closure year']).reindex(mux).reset_index()
+    print(x_df_n)
+    x_df_n = x_df_n.fillna(0)
+    final_list = x_df_n['TotalInvestment'][x_df_n['Financial closure year'].isin(years_x_axis) & (
+        x_df_n['Country'].isin(country_list))].groupby(x_df_n['Country']).apply(list)
+    print("printing final list")
+    print(final_list)
+    for l in final_list:
+        print(len(l))
+    return final_list
+
+
 def plot_graph(years_x_axis, country_list, country_data_y_axis, output_png):
     plot_frame = create_frame(years_x_axis, country_list, country_data_y_axis)
     # style
@@ -37,5 +52,31 @@ def plot_graph(years_x_axis, country_list, country_data_y_axis, output_png):
     plt.close()
 
 
+def plot_3x_graph(years_axis, country_list, country_data_y_axis, ppp_df):
+    final_list = create_frame_3x(years_axis, country_list, country_data_y_axis,ppp_df)
+    # style
+    plt.style.use('seaborn-darkgrid')
 
+    # create a color palette
+    palette = plt.get_cmap('Set1')
+
+    i=0
+    for country in country_list:
+        # fig = plt.figure()
+        # ax1 = fig.add_subplot(111)
+        # ax1.plot(years_axis, country_data_y_axis[i], marker='', color=palette(i), linewidth=1, alpha=0.9, label=country)
+
+        # Add legend
+        # ax1.legend(loc=2, ncol=2)
+        # ax1.savefig('../target/{}.png'.format(i))
+
+        # ax2 = ax1.twinx()
+        # ax2.plot(years_axis, final_list[i], marker='', color=palette(i), linewidth=1, alpha=0.9, label=country)
+
+        # Add legend
+
+        i += 1
+        plt.clf()
+        plt.cla()
+        plt.close()
 
--- src/visualize.py
+++ src/visualize.py
@@ -14,6 +14,15 @@ def filter_gender_stats_csv(inFile, outFile):
     f_data_indicator.to_csv(outFile)
 
 
+def genderFilteredByIndicator(infile, yearColumnFrom, yearColumnTo, indicator, step_data_points):
+    gender_stats_df = pd.read_csv(infile)
+    gender_stats_byRow_df = gender_stats_df[(gender_stats_df["Country Code"].isin(country_list)) & (
+            gender_stats_df["Indicator Code"] == indicator)]
+    gender_stats_byRow_df = gender_stats_byRow_df.fillna(0)
+    gender_stats_byCol_list = gender_stats_byRow_df.loc[:, yearColumnFrom: yearColumnTo : step_data_points].values.tolist()
+    return gender_stats_byCol_list
+
+
 def trigger_scatter_plot(infile, indicator, step_data_points):
     # Plot GDP per capita
     gender_stats_df = pd.read_csv(infile)
@@ -23,6 +32,14 @@ def trigger_scatter_plot(infile, indicator, step_data_points):
             gender_stats_df["Indicator Code"] == indicator)]
     gender_stats_byRow_df = gender_stats_byRow_df.fillna(0)
     gender_stats_byCol_list = gender_stats_byRow_df.loc[:, '1980':'2017': step_data_points].values.tolist()
+
+    print(gender_stats_byCol_list)
+    tmp = genderFilteredByIndicator(infile,str(1980),str(2017),indicator,step_data_points)
+
+    if(gender_stats_byCol_list == tmp):
+        print(True)
+    else:
+        print(False)
     # call plot function to create graph
     plot_scatter.plot_graph(list(range(1980, 2018, step_data_points)), country_list, gender_stats_byCol_list,
                             output_png=indicator)
@@ -54,6 +71,38 @@ def trigger_bubble_plot(infile, indicator):
     plot_bubble_map.plot_bubble_map(total_gdp_df)
 
 
+def trigger_unemloyement_vs_ppp(inFile_ppp, inFile_gender, indicator):
+    dataset_df = pd.DataFrame()
+
+    # filter our and group by values from PPP file
+    f_ppp = pd.read_excel(inFile_ppp)
+    f_ppp = f_ppp.convert_objects(convert_numeric=True)
+    f_ppp['TotalInvestment'] = f_ppp['TotalInvestment'].fillna(0)
+    f_ppp = f_ppp.groupby(['Country', 'Financial closure year'], as_index=False)['TotalInvestment'].sum()
+
+    # Plot GDP per capita
+    gender_stats_df = pd.read_csv(inFile_gender)
+    country_list_name = gender_stats_df['Country Name'][gender_stats_df['Country Code'].isin(country_list)]
+    country_list_name = country_list_name.unique()
+    dataset_df['Country'] = f_ppp['Country'][f_ppp['Country'].isin(country_list_name)].values
+    dataset_df['Financial closure year'] = f_ppp['Financial closure year'][
+        f_ppp['Country'].isin(country_list_name)].values
+    dataset_df['TotalInvestment'] = f_ppp['TotalInvestment'][f_ppp['Country'].isin(country_list_name)].values
+
+    print(dataset_df['Country'])
+    print(dataset_df['Financial closure year'])
+    print(dataset_df['TotalInvestment'])
+
+    # filter based on indicator
+    gender_stats_byRow_df = gender_stats_df[(gender_stats_df["Country Code"].isin(country_list)) & (
+            gender_stats_df["Indicator Code"] == indicator)]
+    gender_stats_byRow_df = gender_stats_byRow_df.fillna(0)
+    gender_stats_byCol_list = gender_stats_byRow_df.loc[:, '2000':'2017'].values.tolist()
+    print(gender_stats_byCol_list)
+    # # call plot function to create graph
+    plot_scatter.plot_3x_graph(list(range(2000, 2018)), country_list_name, gender_stats_byCol_list, dataset_df)
+
+
 def visualize():
     gender_stats_inFile = "../resources/Gender_StatsData.csv"
     gender_stats_outFile = "../target/filtered_gender_stats.csv"
@@ -69,6 +118,11 @@ def visualize():
 
     gdp_indicator = "NY.GDP.MKTP.CD"
     trigger_bubble_plot(gender_stats_outFile, gdp_indicator)
+
+    # read ppp excel
+    unemp_indicator = "SL.UEM.TOTL.FE.ZS"
+    ppp_stats_inFile = "../resources/CustomQuery-8_9_2018.xls"
+    trigger_unemloyement_vs_ppp(ppp_stats_inFile, gender_stats_inFile, unemp_indicator)
 
 
 if __name__ == "__main__":
